4d9b88896f853a27a8677f2f9ab1ca3e553ad407
Agustin Carranza <agustin.carranza@windriver.com>
2023-03-23 23:24:09 -0300

    Se agregan arreglos para compat TLS

---
+++ renew-mongo-cert.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Define variables
+DOMAIN=db.mongow.com
+
+# renew cert
+certbot renew
+
+# combine latest letsencrypt files for mongo
+
+# find latest fullchain*.pem
+newestFull=$(ls -v /etc/letsencrypt/archive/"$DOMAIN"/fullchain*.pem | tail -n 1)
+echo "$newestFull"
+
+# find latest privkey*.pem
+newestPriv=$(ls -v /etc/letsencrypt/archive/"$DOMAIN"/privkey*.pem | tail -n 1)
+echo "$newestPriv"
+
+# combine to mongo.pem
+cat {$newestFull,$newestPriv} | tee /etc/ssl/mongo.pem
+
+# # set rights for mongo.pem
+# chmod 600 /etc/ssl/mongo.pem
+# chown mongodb:mongodb /etc/ssl/mongo.pem
+
+# # restart mongo
+# service mongod restart
+
+cd /home/agustin/tesis/mongodb_docker
+docker-compose down
+docker-compose up -d
+
